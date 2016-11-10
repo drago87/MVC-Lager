faa6926ffe9d17f3a15e7edc1d2f447329726453
drago87 <dr.drago.87@hotmail.com>
2016-11-10 14:13:11 +0100

    Update

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebApplication1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "EFStockItems", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "StockItems", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
